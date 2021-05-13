621077745f5ab1b8fb42bc5854589e17091b8d4a
kuangjian <kuangjian@akulaku.com>
2021-05-13 14:26:01 +0800

    适配ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
 export function createId(): void
+
 export class BuildReleaseId {
   release: string
+  constructor(option: any)
+  apply: (compiler: any) =>any
 }
 
 export function writeFile(release: string): void
